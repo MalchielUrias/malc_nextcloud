b181a25be79cdf278d728c6d6d65d7d5ba2a290e
Malchiel Urias <malchiel.urias@TerraAdminsMBP.lan>
2024-12-24 19:13:10 +0100

    nextcloud docker setup

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -6,3 +6,13 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 sudo apt-get update -y
 sudo apt-get install -y docker-ce
 sudo usermod -aG docker ubuntu  # Add default user to the Docker group
+
+# Install AWS SSM Agent via Snap
+sudo snap install amazon-ssm-agent --classic
+
+# Enable and start the SSM Agent service
+sudo systemctl enable snap.amazon-ssm-agent.amazon-ssm-agent.service
+sudo systemctl start snap.amazon-ssm-agent.amazon-ssm-agent.service
+
+# Verify the SSM Agent status (optional)
+sudo systemctl status snap.amazon-ssm-agent.amazon-ssm-agent.service
